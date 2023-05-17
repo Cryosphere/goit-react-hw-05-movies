47bf33ff8c73de4822a0153485e90ab6bbc6c697
Sergey Shmatok <trycifer@gmail.com>
2023-05-17 23:15:47 +0300

    Fixed App and rename file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,15 @@ import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 
+const HomePage = lazy(() => import('../pages/HomePage'));
 const NotFound = lazy(() => import('../pages/NotFound'));
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<SharedLayout />}></Route>
-
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );

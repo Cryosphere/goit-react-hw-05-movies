54c8a7261da61a9ce97b81738e42bcf20b183be0
Sergey Shmatok <trycifer@gmail.com>
2023-05-18 11:30:27 +0300

    Some fixes of proptypes

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,25 +1,32 @@
 import { Link, useLocation } from 'react-router-dom';
 import { Loader } from '../Loader/Loader';
-
+import PropTypes from 'prop-types';
 
 export const Movies = ({ movies }) => {
-    const location = useLocation();
+  const location = useLocation();
 
-    if (!movies) {
-        return <Loader />;
-    };
+  if (!movies) {
+    return <Loader />;
+  }
 
-    return (
-        <ul>
-        {movies.map(movie => {
-            return (
-            <li key={movie.id}>
-                <Link to={`${movie.id}`} state={{ from: location }}>
-                {movie.title}
-                </Link>
-            </li>
-            );
-        })}
-        </ul>
-    );
+  return (
+    <ul>
+      {movies.map(movie => {
+        return (
+          <li key={movie.id}>
+            <Link to={`${movie.id}`} state={{ from: location }}>
+              {movie.title}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+Movies.propTypes = {
+  movies: PropTypes.arrayOf({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }),
 };
